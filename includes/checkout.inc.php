1dcf5f04a1993ca64c20739c27e116cf06a90c3e
syarifahshah <33937450+syarifahshah@users.noreply.github.com>
2018-01-31 17:54:16 +0800

    Update checkout.inc.php

--- includes/checkout.inc.php
+++ includes/checkout.inc.php
@@ -1,43 +1,54 @@
-<!--SYARIFAH-->
-
 <?php
 
-if (isset($_POST['submit'])) {
+	if (isset($_POST['submit'])) {
 
 	include_once 'dbh.inc.php';
 
-	$first = mysqli_real_escape_string($conn, $_POST['first']);
-	$last = mysqli_real_escape_string($conn, $_POST['last']);
-	$street = mysqli_real_escape_string($conn, $_POST['street']);
-	$unit = mysqli_real_escape_string($conn, $_POST['unit']);
-	$postal = mysqli_real_escape_string($conn, $_POST['postal']);
-	$shipping = mysqli_real_escape_string($conn, $_POST['shipping']);
-	$payment = mysqli_real_escape_string($conn, $_POST['payment']);
+	$name = mysqli_real_escape_string($conn, $_POST['name']);
+	$address = mysqli_real_escape_string($conn, $_POST['address']);
+	$postcode = mysqli_real_escape_string($conn, $_POST['postcode']);
+	$mobile = mysqli_real_escape_string($conn, $_POST['mobile']);
+	$delivery = mysqli_real_escape_string($conn, $_POST['delivery']);
+	$card_no = mysqli_real_escape_string($conn, $_POST['card_no']);
+	$card_name = mysqli_real_escape_string($conn, $_POST['card_name']);
+	$expiry = mysqli_real_escape_string($conn, $_POST['expiry']);
+	$ccv = mysqli_real_escape_string($conn, $_POST['ccv']);
 
 	//error handlers
 	//check for empty fields
-	if (empty($first) || empty($last) || empty($street) || empty($unit) || empty($postal) || empty($shipping) || empty($payment)) {
-		 header("Location: ../checkout.php?checkout=empty");
-		 exit();
-	}  else {
-		//check if inputs are valid
-		//so if inputs are invalid, it will show and error on the url
-		if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last) || !preg_match("/^[a-zA-Z && 0-9]*$/", $street) || !preg_match("/^[0-9]*$/", $unit) || !preg_match("/^[0-9]*$/", $postal)    ) {
-			header("Location: ../checkout.php?checkout=invalid");
+	if  (!preg_match("/^[a-zA-Z]*$/", $name) || 
+		(!preg_match("/^([a-zA-Z 0-9 # -])+$/", $address) || 
+		(!preg_match("/^([0-9]){6}$/", $postcode) || 
+		(!preg_match("/^([0-9]){8}$/", $mobile) ||
+		(!preg_match("/^([a-zA-Z]){8}$/", $delivery) || 
+		(!preg_match("/^([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}$/", $card_no) || 
+		(!preg_match("/^([a-zA-Z])$/", $card_name) || 
+		(!preg_match("/^([0-9]){2}([-])([0-9]){4}$/", $expiry) || 
+		(!preg_match("/^([0-9]){3}$/", $ccv)) 
+
+		{
+			header("Location: ../shipping.php?checkout=invalid");
 		 	exit();
-		} else {
+		} 
+
+		else {
 			//only when everything is checked and validated,
 			//then will insert user into database
-			$sql = "INSERT INTO checkout (checkout_first, checkout_last, checkout_street, checkout_unit, checkout_postal, checkout_shipping, checkout_payment) VALUES ('$first', '$last', '$street', '$unit', '$postal', '$shipping', '$payment');";
+			$sql = "INSERT INTO shipping (name, address, postcode, mobile, delivery, card_no, card_name, expiry, ccv) VALUES ('$name', '$address', '$postcode', '$mobile', '$delivery', '$card_no', '$card_name', '$expiry', '$ccv');";
+			
 			mysqli_query($conn, $sql);
 			header("Location: ../summary.php");
 		 	exit();
 
-
 		}
 	}
 
-} else {
- 	header("Location: ../checkout.php");
+else {
+ 	header("Location: ../summary.php");
 	exit();
 }
+
+?>
+
+
+
