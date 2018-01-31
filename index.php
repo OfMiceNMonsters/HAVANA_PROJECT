b25d26bc76d746c073ea912ea2352bf9c913f232
NK <33948158+OfMiceNMonsters@users.noreply.github.com>
2018-01-31 17:30:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,113 +1,57 @@
-<!--Khairunnisa-->
-
-
-<!-- This is to include the header.php into the main catalogue page-->
 <?php
-	include_once 'header.php';
+	include_once 'header.php';  
+ $connect = mysqli_connect("localhost", "root", "", "havana");
 ?>
 
 <!DOCTYPE html>
 <html>
 <title>HAVANA</title>
-<section class="container"> <!-- I used a container to place the main content inside it -->
+<section class="container">
 
-		<div class="content">
+    <div class="content">
 
-			<br>
-			<br>
-			<br>
-			<div class="row"> <!-- In order to make a "table" of the catalogue, i used rows and columns to store the listings in -->
-				<div class="column" style="background-color: white;"> <!-- Initially, i made a filled border to be in the container to have a white background but found that an easier way to achieve that is to utilize the column background instead-->
-					<div class="container2">
-  					<img src="https://i.pinimg.com/736x/60/2b/2d/602b2d86f845d4bef68bb85d31592576--vintage-nike-windbreaker-nike-windbreaker-jacket.jpg" alt="Avatar" class="image" style="width:100%">
-  					<div class="middle">
-    			<div class="text">Size: L // $43.00</a></div> <!-- When the user hovers over the photo, the details of the listing will appear -->
-  				</div>
-				</div>
-				<br>
-				<p>Vintage Nike Windbreaker</p>
-				<br>
-				<div class="button2"><div class="text2">Add to Cart</div></div> <!-- This buttons allows the user to add this certain listing into their cart-->
-				</div>
-				<div class="column" style="background-color: white;">
-					<div class="container2">
-  					<img src="https://i.pinimg.com/736x/d3/12/8d/d3128d2870cdf43d064285174582c9b3--vintage-nike-windbreaker-nike-windbreaker-jacket.jpg" alt="Avatar" class="image" style="width:100%">
-  					<div class="middle">
-    			<div class="text">Size: M // $49.00</a></div>
-  				</div>
-				</div>
-				<br>
-				<p>Vintage Nike Windbreaker</p>
-				<br>
-				<div class="button2"><div class="text2">Add to Cart</div></div>
-				</div>
-				<div class="column" style="background-color: white;">
-					<div class="container2">
-  					<img src="https://i.pinimg.com/736x/0e/bd/ba/0ebdba406d4071dff993ae5b9ea48f72--black-nike-windbreaker-nike-vintage-windbreaker.jpg" alt="Avatar" class="image" style="width:100%">
-  					<div class="middle">
-    			<div class="text">Size: S // $45.00</a></div>
-  				</div>
-				</div>
-				<br>
-				<p>Vintage Nike Windbreaker</p>
-				<br>
-				<div class="button2"><div class="text2">Add to Cart</div></div>
-				</div>
+      <br>
+      <br>
+      <br> 
 
-				<br>
-				<br>
-				<br>
-				<div class="column" style="background-color: white;">
-					<div class="container2">
-  					<img src="https://i.pinimg.com/originals/64/6b/da/646bda5ad35d6a050f9a287547944f40.jpg" alt="Avatar" class="image" style="width:100%">
-  					<div class="middle">
-    			<div class="text">Size: L // $50.00</a></div>
-  				</div>
-				</div>
-				<br>
-				<p>Vintage Nike Windbreaker</p>
-				<br>
-				<div class="button2"><div class="text2">Add to Cart</div></div>
-				</div>
-				<div class="column" style="background-color: white;">
-					<div class="container2">
-  					<img src="http://picture-cdn.wheretoget.it/rk09yf-i.jpg" alt="Avatar" class="image" style="width:100%">
-  					<div class="middle">
-    			<div class="text">Size: S // $33.00</a></div>
-  				</div>
-				</div>
-				<br>
-				<p>Vintage Nike Windbreaker</p>
-				<br>
-				<div class="button2"><div class="text2">Add to Cart</div></div>
-				</div>
-				<div class="column" style="background-color: white;">
-					<div class="container2">
-  					<img src="https://i.pinimg.com/736x/a5/03/17/a5031783c69fc708c5d4da45e45a3083.jpg" alt="Avatar" class="image" style="width:100%">
-  					<div class="middle">
-    			<div class="text">Size: M // $48.00</a></div>
-  				</div>
-				</div>
-				<br>
-				<p>Vintage Vans Sweatshirt</p>
-				<br>
-				<div class="button2"><div class="text2">Add to Cart</div></div>
-				</div>
-
-			</div>
+       <?php  
+                $query = "SELECT * FROM tbl_product ORDER BY id ASC";  
+                $result = mysqli_query($connect, $query);  
+                if(mysqli_num_rows($result) > 0)  
+                {  
+                     while($row = mysqli_fetch_array($result))  
+                     {  
+                ?> 
+                <div class="column" style="background-color: white;">
+                <div class ="row">  
+                     <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
+                          <br>
+                          <br>  
+                               <img src="<?php echo $row["image"]; ?>" class="img-responsive" alt="Avatar" class="image" style="width:100%"/><br />  
+                               <h4 class="text-info"><?php echo $row["name"]; ?></h4>  
+                               <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>  
+                               <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />  
+                               <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />  
+                               <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />  
+                      </div>
+                      </div>
+                  </div>
+                     </form>  
+             </div> 
+                <?php  
+                     }  
+                }  
+                ?>   
 
 
-		</div>
-		</div>
-
-		<?php
-			if (isset($_SESSION['u_id'])) {
-				echo "You are logged in!";
-			}
-		?>
-	</div>
+    <?php
+      if (isset($_SESSION['u_id'])) {
+        echo "You are logged in!";
+      }
+    ?>
+  </div>
 </section>
 
 <?php
-	include_once 'footer.php';
+  include_once 'footer.php';
 ?>
