c278ef04c6ca71ace52491ca75d96cf8297aef16
syarifahshah <33937450+syarifahshah@users.noreply.github.com>
2018-01-31 17:50:59 +0800

    Update summary.php

--- summary.php
+++ summary.php
@@ -1,5 +1,3 @@
-<!--SYARIFAH-->
-
 <?php
 	include_once 'header.php';
 ?>
@@ -9,12 +7,26 @@
 		<!--to make it appear in the white border-->
 		<div id="example1">
 			<!--just to show that you're at main page-->
-			<h2>Thank you for purchasing with us! </h2>
-			<form class="summary-form" method="POST">
+			<link rel="stylesheet" type="text/css" href="style.css">
+			<table class="checkout">
+				<tr>
+				<th>THANK YOU FOR SHOPPING WITH US!</th>
+				</tr>
+			</table>
+
+			<br/><br/>
+
+			<form class="bye" method="POST">
+					<label>Feedback / Suggestions:</label>
+					<br/><br/>
+					<textarea rows="4" cols="50" name="comment" form="usrform">Please leave a feedback for us..</textarea>
+
+					<br/><br/><br/><br/>
+
 					<!--so once submitted will go back to catalogue-->
 					<button type="submit" name="submit"><a href= "index.php">Browse Catalogue</a></button>
 			</form>
-
+		
 		</div>
 	</div>
 </section>
