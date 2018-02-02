4e42f9cb55f48ee2b5882c9903aee765ff1c5279
syarifahshah <33937450+syarifahshah@users.noreply.github.com>
2018-02-02 15:59:11 +0800

    Add files via upload

---
+++ shippingname.php
@@ -0,0 +1,33 @@
+<?php
+	include_once 'afterlogin.php';
+?>
+
+<section class="main-container">
+	<div class="main-wrapper">
+		<!--to make it appear in the white border-->
+		<div id="example1">
+			<!--just to show that you're at main page-->
+			<link rel="stylesheet" type="text/css" href="style.css">
+			<table class="checkout">
+				<tr>
+				<th>ERROR 404: DETECTED</th>
+				</tr>
+			</table>
+
+			<br/><br/>
+
+			<form class="bye" method="POST">
+					<label>Error Detected: Please retype your NAME input. Only use letters (a-z,A-Z).</label>
+					<br/><br/><br/><br/>
+
+					<!--so once submitted will go back to catalogue-->
+					<button type="submit" name="submit"><a href= "shipping.php">BACK</a></button>
+			</form>
+		
+		</div>
+	</div>
+</section>
+
+<?php
+	include_once 'footer.php';
+?>
