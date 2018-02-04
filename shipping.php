43bae00671a5446e4ccdee3d71590989e49a0106
syarifahshah <33937450+syarifahshah@users.noreply.github.com>
2018-02-05 01:00:55 +0800

    Update shipping.php

--- shipping.php
+++ shipping.php
@@ -1,43 +1,52 @@
 <?php
+	// include codes for navbar
 	include_once 'afterlogin.php';
 ?>
 
+<!--background classes-->
 <section class="main-container">
 	<div class="main-wrapper">
+		<!--white main background-->
 		<div id="example1">
 			<!--start actual codes-->
 			<!--top label-->
 			<link rel="stylesheet" type="text/css" href="style.css">
 			<table class="checkout">
 				<tr>
+				<!--1st table header-->
 				<th>SHIPPING INFORMATION</th>
 				</tr>
 			</table>
-
+				<!--after this form submits, go to prepared.inc.php-->
 				<form class="checkout-form" action="includes/prepared.inc.php" method="POST">
+					<!--input and label for name-->
 					<label>Name:</label>
 					<input type="text" name="name" placeholder="Nur Syarifah Shah" required>
 
 					<br/>
 					<br/>
-
+					
+					<!--input and label for address-->
 					<label>Address:</label>
 					<input type="text" name="address" placeholder="Blk 103A Edgefield Plains #12-192" required>
 					
 					<br/>
 					<br/>
 
+					<!--input and label for postcode-->
 					<label>Postcode:</label>
 					<input type="text" name="postcode" placeholder="823103" required>
 
 					<br/>
 					<br/>
 
+					<!--input and label for mobile-->
 					<label>Mobile Number:</label>
 					<input type="text" name="mobile" placeholder="93781209" required>
 
 					<br/><br/><br/>
 
+					<!--2nd table header-->
 					<table class="checkout">
 						<tr>
 						<th>DELIVERY OPTION</th>
@@ -49,30 +58,35 @@
 
 					<br/>
 					<br/>
-
+					
+					<!--input and label for delivery-->
 					<label>Shipping:</label>
 					<input type="text" name="delivery" placeholder="Enter Standard or Express" required>
 
 					<br/><br/><br/><br/>
 
+					<!--3rd table header-->
 					<table class="checkout">
 						<tr>
 						<th>PAYMENT DETAILS</th>
 						</tr>
 					</table>
 
+					<!--input and label for card_no-->
 					<label>Card Number:</label>
 					<input type="text" name="card_no" placeholder="1234-5678-9012-1314" required>
 
 					<br/>
 					<br/>
 
+					<!--input and label for card_name-->
 					<label>Name on Card:</label>
 					<input type="text" name="card_name" placeholder="Nur Syarifah" required>
 
 					<br/>
 					<br/>
 
+					<!--input and label for name expiry-->
 					<label>Expiry Date:</label>
 					<input type="text" name="expiry" placeholder="12-2022" required>
 					<!--<label></label>
@@ -81,11 +95,13 @@
 					<br/>
 					<br/>
 
+					<!--input and label for ccv-->
 					<label>CCV/CVV:</label>
 					<input type="text" name="ccv" placeholder="123" required>
 
 					<br/><br/><br/><br/><br/><br/><br/><br/><br/>
 
+					<!--button to link to prepared.inc.php-->
 					<button type="submit" name="submit">SUBMIT</button>
 					
 			</form>
