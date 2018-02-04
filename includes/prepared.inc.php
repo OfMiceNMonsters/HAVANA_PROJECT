825996ebbcb6d8e7497f3009ac5f7c1da4439242
syarifahshah <33937450+syarifahshah@users.noreply.github.com>
2018-02-05 01:10:19 +0800

    Update prepared.inc.php

--- includes/prepared.inc.php
+++ includes/prepared.inc.php
@@ -1,91 +1,133 @@
 <?php
 
+	// to be called when _POST for xss prevention
+	function strip_tag($string) 
+	{ 
+		// strip html & php tags
+		$string = strip_tags($string);
+		// strip control characters
+		return preg_replace('/[[:punct:]]/', ' ', $string);
+	}
+	
+	// when submit button is posted
 	if (isset($_POST['submit'])) {
 
-	include_once 'dbh.inc.php';
+	// initialize the localhost -> root -> havana
+	$dbServername = "localhost";
+	$dbUsername = "root";
+	$dbPassword = "";
+	$dbName = "havana";
 
-	$name = $_POST['name'];
-	$address = $_POST['address'];
-	$postcode = $_POST['postcode'];
-	$mobile = $_POST['mobile'];
-	$delivery = $_POST['delivery'];
-	$card_no = $_POST['card_no'];
-	$card_name = $_POST['card_name'];
-	$expiry = $_POST['expiry'];
-	$ccv = $_POST['ccv'];
+	// link to database
+	$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+	
+	//post the inputs and add the strip tags for xss prevention
+	$name =  strip_tag($_POST['name']);
+	$address = strip_tag($_POST['address']);
+	$postcode = strip_tag($_POST['postcode']);
+	$mobile = strip_tag($_POST['mobile']);
+	$delivery = strip_tag($_POST['delivery']);
+	$card_no = strip_tag($_POST['card_no']);
+	$card_name = strip_tag($_POST['card_name']);
+	$expiry = strip_tag($_POST['expiry']);
+	$ccv = strip_tag($_POST['ccv']);
 
-	//error handlers
-	//check for empty fields
+	// error handlers
+	// name input can only have letters
 	if  (!preg_match("/^[a-zA-Z ]*$/", $name))
 		{
+			// if never follow regex, go to shippingname.php error page
 			header("Location: ../shippingname.php");
 		 	exit();
 		} 
 		
+		// address input can only have letters, numbers and # | -
 		else if (!preg_match ("/^([a-zA-Z 0-9 # - ])+$/", $address))
 			{
+				// if never follow regex, go to shippingaddress.php error page
 				header("Location: ../shippingaddress.php");
 				exit();
 			}
 			
+			// postcode input can only have numbers and must be 6 digits
 			else if (!preg_match ("/^([0-9]){6}$/", $postcode))
 				{
+					// if never follow regex, go to shippingpostcode.php error page
 					header("Location: ../shippingpostcode.php");
 					exit();
 				}
 	
+				// mobile input can only have numbers and must be 8 digits
 				else if (!preg_match ("/^([0-9]){8}$/", $mobile))
 					{
+						// if never follow regex, go to shippingmobile.php error page
 						header("Location: ../shippingmobile.php");
 						exit();
 					}
 					
+					// delivery input can only be Standard or Express 
 					else if (!preg_match ("/^(Standard|Express)$/", $delivery))
 						{
+							// if never follow regex, go to shippingdelivery.php error page
 							header("Location: ../shippingdelivery.php");
 							exit();
 						}
 						
+						// card_no input can only have numbers and must be 16 digits
 						else if (!preg_match ("/^([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}$/", $card_no))
 							{
+								// if never follow regex, go to shippingcardno.php error page
 								header("Location: ../shippingcardno.php");
 								exit();
   							}
 							
+							// card_name input can only have letters
 							else if (!preg_match ("/^[a-zA-Z ]*$/", $card_name))
 								{
+									// if never follow regex, go to shippingcardname.php error page
 									header("Location: ../shippingcardname.php");
 									exit();
 								}
 								
+								// expiry input can only have numbers in the format mm-yyyy
 								else if (!preg_match ("/^([0-9]){2}([-])([0-9]){4}$/", $expiry))
 									{
+										// if never follow regex, go to shippingexpiry.php error page
 										header("Location: ../shippingexpiry.php");
 										exit();
 									}
 									
+									// ccv input can only have numbers and must be 3 digits
 									else if (!preg_match ("/^([0-9]){3}$/", $ccv))
 										{
+											// if never follow regex, go to shippingccv.php error page
 											header("Location: ../shippingccv.php");
 											exit();
 										}
 										
-										else {
-											//only when everything is checked and validated,
-											//then will insert user into database
+										else 
+										{
+											// only when everything is checked and validated,
+											// then will insert user into database
 											$sql = "INSERT INTO shipping (name, address, postcode, mobile, delivery, card_no, card_name, expiry, ccv) 
 													VALUES ('$name', '$address', '$postcode', '$mobile', '$delivery', '$card_no', '$card_name', '$expiry', '$ccv');";
+											
+											// once input inserted into database, drop rows inside the cart 
+											// ASK WHY TRUNCATE CANNOT WORK!!!!!
 											$sql = "TRUNCATE TABLE cart4";
 											mysqli_query($conn, $sql);
+											// if everything is ok, link to summary page
 											header("Location: ../summary.php");
+											// go out of the loop
 											exit();
 										}
 										
+											
 	}
-
 	
 else {
- 	header("Location: ../shipping.php");
+	// if nothing is clicked etc, stay on the shipping.php page 
+	header("Location: ../shipping.php");
 	exit();
 }
 
