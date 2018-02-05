1c569048a2b1d154d6c4c3c25b85a46efe94486f
alliephant <34224184+alliephant@users.noreply.github.com>
2018-02-05 12:43:11 +0800

    Add files via upload

---
+++ cartempty.php
@@ -0,0 +1,38 @@
+<!--alison-->
+
+<?php
+  include_once 'header.php';
+  include_once 'includes/dbh.inc.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+<title>Havana </title>
+<head>
+		<link rel="stylesheet" type="text/css" href="cart.css">
+</head>
+<body>
+
+		<!--white background-->
+		<div class="container">
+		<div class="content">
+		<div id="example1">
+<br>
+<br>
+<br>
+<p align="center"> <strong> <big>  SHOPPING CART </big> </strong> </p>
+<br>
+
+<div align="center">
+  <p> Shopping cart is empty! Go shop it out! :) </p>
+  <br>
+  <img src="https://www.dontpayfull.com/blog/wp-content/uploads/2016/04/Secret-Shopping-Tricks.jpg">
+  <br>
+  <button type="button" onclick= "location.href = '/mainafterlogin.php'">Catalogue baby~</button>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
