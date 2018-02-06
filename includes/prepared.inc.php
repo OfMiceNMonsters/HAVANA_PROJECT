742ed9cdf259785195097b7715e931a0e18fec58
syarifahshah <33937450+syarifahshah@users.noreply.github.com>
2018-02-06 12:34:02 +0800

    Update prepared.inc.php

--- includes/prepared.inc.php
+++ includes/prepared.inc.php
@@ -21,6 +21,31 @@
 	// link to database
 	$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 	
+	// encrypt card_no
+	// $inputtext = $_POST['card_no'];
+	// $key = 'SecretKeySecretK';
+	
+	// verify the initialization vector
+	// $iv_size = mcrypt_get_iv_size (MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+	
+	// create the initialization vector with random numbers 
+	// $iv = 12345;
+	// $encryptedtext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $inputtext, MCRYPT_MODE_ECB, $iv);
+	
+	//function encrypt($decrypted, $card_no, $salt='!kQm*fF3pXe1Kbm%9') { 
+	// Build a 256-bit $key which is a SHA256 hash of $salt and $card_no.
+	//$key = hash('SHA256', $salt . $card_no, true);
+	// Build $iv and $iv_base64.  We use a block size of 128 bits (AES compliant) and CBC mode.  (Note: ECB mode is inadequate as IV is not used.)
+	//srand(); $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC), MCRYPT_RAND);
+	//if (strlen($iv_base64 = rtrim(base64_encode($iv), '=')) != 22) return false;
+	// Encrypt $decrypted and an MD5 of $decrypted using $key.  MD5 is fine to use here because it's just to verify successful decryption.
+	//$encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $decrypted . md5($decrypted), MCRYPT_MODE_CBC, $iv));
+	// We're done!
+	//return $iv_base64 . $encrypted;
+	
+		
+		
+		
 	//post the inputs and add the strip tags for xss prevention
 	$name =  strip_tag($_POST['name']);
 	$address = strip_tag($_POST['address']);
@@ -74,7 +99,8 @@
 						}
 						
 						// card_no input can only have numbers and must be 16 digits
-						else if (!preg_match ("/^([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}$/", $card_no))
+						else if (!preg_match ("/^([0-9]){16}$/", $card_no))
+							//else if (!preg_match ("/^([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}$/", $card_no))
 							{
 								// if never follow regex, go to shippingcardno.php error page
 								header("Location: ../shippingcardno.php");
@@ -90,7 +116,8 @@
 								}
 								
 								// expiry input can only have numbers in the format mm-yyyy
-								else if (!preg_match ("/^([0-9]){2}([-])([0-9]){4}$/", $expiry))
+								else if (!preg_match ("/^([0-9]){6}$/", $expiry))
+									//else if (!preg_match ("/^([0-9]){2}([-])([0-9]){4}$/", $expiry))
 									{
 										// if never follow regex, go to shippingexpiry.php error page
 										header("Location: ../shippingexpiry.php");
@@ -112,10 +139,28 @@
 											$sql = "INSERT INTO shipping (name, address, postcode, mobile, delivery, card_no, card_name, expiry, ccv) 
 													VALUES ('$name', '$address', '$postcode', '$mobile', '$delivery', '$card_no', '$card_name', '$expiry', '$ccv');";
 											
+											//$query = $conn->prepare("insert into shipping (name, address, postcode, mobile, delivery, card_no, card_name, expiry, ccv) 
+													//VALUES ('$name', '$address', '$postcode', '$mobile', '$delivery', '$card_no', '$card_name', '$expiry', '$ccv');");
+												//$query->bind_param("ssiisisii", $name, $address, $postcode, $mobile, $delivery, $card_no, $card_name, $expiry, $ccv);
+												//$query->execute();
+											
 											// once input inserted into database, drop rows inside the cart 
 											// ASK WHY TRUNCATE CANNOT WORK!!!!!
-											$sql = "TRUNCATE TABLE cart4";
+											//$sql = "TRUNCATE TABLE cart4";
 											mysqli_query($conn, $sql);
+											
+											// instantiate our PDO object and create a new MySQL connection.
+											$pdo = new PDO('mysql:host=localhost;dbname=havana', 'root', '');
+											 
+											// SQL statement to delete all rows in cart4
+											$sql = "TRUNCATE TABLE `cart4`";
+											 
+											// prepare the SQL query.
+											$statement = $pdo->prepare($sql);
+											 
+											// execute the statement.
+											$statement->execute();
+											
 											// if everything is ok, link to summary page
 											header("Location: ../summary.php");
 											// go out of the loop
@@ -124,6 +169,7 @@
 										
 											
 	}
+			
 	
 else {
 	// if nothing is clicked etc, stay on the shipping.php page 
