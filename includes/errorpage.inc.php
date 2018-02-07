1d6ca7a6d29484057fe3d95f76baeeb8d39691c5
NigouNigouNigou <34224154+NigouNigouNigou@users.noreply.github.com>
2018-02-07 17:50:49 +0800

    Add files via upload

---
+++ includes/errorpage.inc.php
@@ -0,0 +1,30 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HAVANA</title>
+	<link rel="stylesheet" type="text/css" href="errorpage.css">
+</head>
+<body>
+
+<header>
+	<nav>
+		<div class="main-wrapper">
+			<ul>
+				<li><b><a href="index.php">HAVANA</a></b></li>
+			</ul>
+			<div class="nav-login">
+
+					<a href="signup.php">Sign up</a>';
+					<a href="aboutus.php">About Us</a>
+					<a href="contactus.php">Contact Us</a>
+					<a href="cart.php">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cart</a>
+					<a href="settings.php">Settings </a> 
+					<a href="faq.php">FAQ</a>
+			</div>
+		</div>
+	</nav>
+</header>
