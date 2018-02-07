3dda3ae467f0ddbd7aa927f8bb6dd65e34742803
syarifahshah <33937450+syarifahshah@users.noreply.github.com>
2018-02-08 03:09:06 +0800

    Update summary.php

--- summary.php
+++ summary.php
@@ -19,21 +19,11 @@
 
 			<br/><br/>
 
-			<form class="summary-form" method="POST">
-					<label>Feedback / Suggestions:</label>
-					<br/><br/>
-					<!--make a text area-->
-					<textarea rows="4" cols="50" name="comment" form="usrform">Please leave a feedback for us..</textarea>
-
-					<br/><br/><br/><br/>
-
-					<!--if don't want enter feedback, just go catalogue-->
-					<button type="submit" name="submit"><a href= "index.php">Browse Catalogue</a></button>
+	
+			<!--just go catalogue-->
+			<button type="submit" name="submit"><a href= "main_afterlogin.php">Browse Catalogue</a></button>
 					
-					<!--submit button for feedback, auto go to catalogue-->
-					<button type="submit" name="submit"><a href= "index.php">Submit</a></button>
-					
-			</form>
+		
 			
 		</div>
 	</div>
