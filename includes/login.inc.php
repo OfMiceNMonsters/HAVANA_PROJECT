cfb25920933e08cb59a3aba99e7e20b37cd8f3b7
NigouNigouNigou <34224154+NigouNigouNigou@users.noreply.github.com>
2018-02-07 17:45:19 +0800

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,21 +1,11 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "havana");;
-
-function strip_tag($string) { 
-
-	// strip html & php tags
-	$string = strip_tags($string);
-	// strip control characters
-	return preg_replace('/[[:punct:]]/', ' ', $string);
-
-}
-
 if (isset($_POST['submit'])) {
 	
+	$conn = mysqli_connect("localhost", "root", "", "havana");
 
-	$uid = strip_tags($_POST['uid']);
-	$pwd = strip_tags($_POST['pwd']);
+	$uid = $_POST['uid'];
+	$pwd = $_POST['pwd'];
 
 	//Error handlers
 	//Check if inputs are empty
@@ -23,7 +13,7 @@ if (isset($_POST['submit'])) {
 		header("Location: ../index.php");
 		exit();
 	} else {
-		$sql = "SELECT * FROM usersandemail WHERE user_uid='$uid' OR user_email='$uid'";
+		$sql = "SELECT * FROM usersandemail WHERE username='$uid' OR user_email='$uid'";
 		$result = mysqli_query($conn, $sql);
 		$resultCheck = mysqli_num_rows($result);
 		if ($resultCheck < 1) {
@@ -32,17 +22,17 @@ if (isset($_POST['submit'])) {
 		} else {
 			if ($row = mysqli_fetch_assoc($result)) {
 				//De-hashing the password
-				$hashedPwdCheck = password_verify($pwd, $row['user_pwd']);
+				$hashedPwdCheck = password_verify($pwd, $row['user_password']);
 				if ($hashedPwdCheck == false) {
-					echo 'Please try again, invalid username or password. <a href="index.php">Return to Homepage </a> ';
+					echo 'Please try again, invalid username or password. <a href="<a href=C:\xampp\htdocs\prac\LOGIN_2_SYA\LOGIN_2_SYA\rootfolder\KHAI_HOMEPAGE\LOGIN_khai\rootfolder\index.php">Return to Homepage </a> ';
 					exit();
 				} elseif ($hashedPwdCheck == true) {
 					//Log in the user here
 					$_SESSION['u_id'] = $row['user_id'];
-					$_SESSION['u_first'] = $row['user_first'];
-					$_SESSION['u_last'] = $row['user_last'];
+					$_SESSION['u_first'] = $row['user_firstname'];
+					$_SESSION['u_last'] = $row['user_lastname'];
 					$_SESSION['u_email'] = $row['user_email'];
-					$_SESSION['u_uid'] = $row['user_uid'];
+					$_SESSION['u_uid'] = $row['username'];
 					header("Location: ../main_afterlogin.php");
 					exit();
 				}
