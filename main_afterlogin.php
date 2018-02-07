63ad4b0c3bf47422078f062198b2d4eb58f2c248
NigouNigouNigou <34224154+NigouNigouNigou@users.noreply.github.com>
2018-02-07 17:03:41 +0800

    Update main_afterlogin.php

--- main_afterlogin.php
+++ main_afterlogin.php
@@ -1,6 +1,6 @@
 <?php
 	include_once 'afterlogin.php';
-	$connect = mysqli_connect("localhost", "root", "", "havana");
+	$conn = mysqli_connect("localhost", "root", "", "havana");
 ?>
 
 
@@ -39,7 +39,7 @@ if(isset($_POST["insert"]))
 
        <?php  
                 $query = "SELECT * FROM tbl_product ORDER BY id ASC";  
-                $result = mysqli_query($connect, $query);  
+                $result = mysqli_query($conn, $query);  
                 if(mysqli_num_rows($result) > 0)  
                 {  
                      while($row = mysqli_fetch_array($result))  
@@ -53,8 +53,10 @@ if(isset($_POST["insert"]))
                                <img src="<?php echo $row["image"]; ?>" class="img-responsive" alt="Avatar" class="image" style="width:100%"/><br />  
                                <h4 class="text-info"><?php echo $row["name"]; ?></h4>  
                                <h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>  
-                               <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />  
-                               <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />  
+                               <input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
+                               <input type="hidden" name="hidden_image" value="<?php echo $row["image"]; ?>" />  
+                               <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" /> 
+                               <input type="hidden" name="hidden_size" value="<?php echo $row["size"]; ?>" /> 
                                <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />  
                       </div>
                       </div>
@@ -64,10 +66,10 @@ if(isset($_POST["insert"]))
                 <?php  
                      }  
                 }  
-                ?>      
+                ?>     
 
 		<?php
-			if (isset($_SESSION['u_id'])) {
+			if (isset($_SESSION['user_id'])) {
 				echo "You are logged in!";
 			}
 		?>
