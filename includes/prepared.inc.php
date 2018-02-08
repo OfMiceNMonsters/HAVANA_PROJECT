d9c0f4ef40e4b5a2bef1fc4d3027fe770a0ab444
syarifahshah <33937450+syarifahshah@users.noreply.github.com>
2018-02-08 15:34:29 +0800

    Update prepared.inc.php

--- includes/prepared.inc.php
+++ includes/prepared.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-	// to be called when _POST for xss prevention
 	function strip_tag($string) 
 	{ 
 		// strip html & php tags
@@ -32,7 +31,7 @@
 	$expiry = htmlspecialchars(strip_tags($_POST['expiry']));
 	$ccv = htmlspecialchars(strip_tags($_POST['ccv']));
 
-	// regular expression
+	// error handlers
 	// name input can only have letters
 	if  (!preg_match("/^[a-zA-Z ]*$/", $name))
 		{
@@ -75,76 +74,81 @@
 						
 						// card_no input can only have numbers and must be 16 digits
 						else if (!preg_match ("/^([0-9]){16}$/", $card_no))
-							//else if (!preg_match ("/^([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}$/", $card_no))
+						//else if (!preg_match ("/^([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}([-])([0-9]){4}$/", $card_no))
 							{
 								// if never follow regex, go to shippingcardno.php error page
 								header("Location: ../shippingcardno.php");
 								exit();
-  							}
-							
-							// card_name input can only have letters
-							else if (!preg_match ("/^[a-zA-Z ]*$/", $card_name))
+							}
+	
+								// card_name input can only have letters
+								else if (!preg_match ("/^[a-zA-Z ]*$/", $card_name))
 								{
 									// if never follow regex, go to shippingcardname.php error page
 									header("Location: ../shippingcardname.php");
 									exit();
 								}
-								
-								// expiry input can only have numbers in the format mm-yyyy
-								else if (!preg_match ("/^([0-9]){6}$/", $expiry))
+											
+									// expiry input can only have numbers in the format mm-yyyy
+									else if (!preg_match ("/^([0-9]){6}$/", $expiry))
 									//else if (!preg_match ("/^([0-9]){2}([-])([0-9]){4}$/", $expiry))
 									{
 										// if never follow regex, go to shippingexpiry.php error page
 										header("Location: ../shippingexpiry.php");
 										exit();
 									}
-									
-									// ccv input can only have numbers and must be 3 digits
-									else if (!preg_match ("/^([0-9]){3}$/", $ccv))
+
+										// ccv input can only have numbers and must be 3 digits
+										else if (!preg_match ("/^([0-9]){3}$/", $ccv))
+										//else if (!preg_match ("/^([0-9]){2}([-])([0-9]){4}$/", $expiry))
 										{
-											// if never follow regex, go to shippingccv.php error page
+											// if never follow regex, go to shippingexpiry.php error page
 											header("Location: ../shippingccv.php");
 											exit();
 										}
-										
-										else 
-										{
-											// only when everything is checked and validated,
-											// then will insert user into database
-											// prepare statement
-											$query = "INSERT INTO shipping (name, address, postcode, mobile, delivery, card_no, card_name, expiry, ccv) VALUES (?,?,?,?,?,?,?,?,?)";
-											$statement = $conn->prepare($query);
-											// binding parameters
-											$statement->bind_param("ssiisisii", $name, $address, $postcode, $mobile, $delivery, $card_no, $card_name, $expiry, $ccv);
+													
+											else 
+											{
+												$hashed = password_hash($ccv, PASSWORD_BCRYPT);
+												
+												$hashedcard = $card_no;
+												$key = 'SecretKeySecretK';
+												
+												$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+												//create IV with random numbers
+												$iv = 88888;
+												$encryptedtext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $hashedcard, MCRYPT_MODE_ECB, $iv);
+
+												//encrypted hex text
+												// echo "Encrypted text: ".bin2hex($encryptedtext)."<br>";
+												$decryptedtext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $encryptedtext, MCRYPT_MODE_ECB, $iv);
 											
-											// run the query
-											$statement->execute();
-											
-											// to check any errors
-											print $statement->error;
-											
-											// once input inserted into database, drop rows inside the cart 
-											// instantiate our PDO object and create a new MySQL connection.
-											$pdo = new PDO('mysql:host=localhost;dbname=havana', 'root', '');
-											 
-											// SQL statement to delete all rows in cart4
-											$sql = "TRUNCATE TABLE `cart`";
-											 
-											// prepare the SQL query.
-											$query = $pdo->prepare($sql);
-											 
-											// execute the statement.
-											$query->execute();
-											
-											// if everything is ok, link to summary page
-											header("Location: ../summary.php");
-											// go out of the loop
-											exit();
-										}
-										
-											
-	}
-			
+												$query = "INSERT INTO shipping (name, address, postcode, mobile, delivery, encrypted_card_no, card_name, expiry, encrypted_ccv) 
+															VALUES (?,?,?,?,?,?,?,?,?)";
+												$statement = $conn->prepare($query);
+												$statement->bind_param("ssiisssis", $name, $address, $postcode, $mobile, $delivery, $encryptedtext, $card_name, $expiry, $hashed);
+												$statement->execute(); 
+
+												// once input inserted into database, drop rows inside the cart 
+												// instantiate our PDO object and create a new MySQL connection.
+												$pdo = new PDO('mysql:host=localhost;dbname=havana', 'root', '');
+																	
+												// SQL statement to delete all rows in cart4
+												$sql = "TRUNCATE TABLE `cart`";
+																		 
+												// prepare the SQL query.
+												$query = $pdo->prepare($sql);
+																 
+												// execute the statement.
+												$query->execute();
+																	
+												// if everything is ok, link to summary page
+												header("Location: ../summary.php");
+												// go out of the loop
+												exit();
+											}				
+													
+	}			
 	
 else {
 	// if nothing is clicked etc, stay on the shipping.php page 
