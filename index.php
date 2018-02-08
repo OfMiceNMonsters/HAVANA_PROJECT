2ade169e2cf4722f10288e6120c97b8c71ecf4fa
alliephant <34224184+alliephant@users.noreply.github.com>
2018-02-08 11:17:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,44 @@
 <?php
 	include_once 'header.php';  
- $connect = mysqli_connect("localhost", "root", "", "havana");
+ $conn = mysqli_connect("localhost", "root", "", "havana");
 ?>
 
+<?php
+/*
+//session management. In every session, generate its own token to check for time validity  
+//if the posted token is the session id given
+if($_POST['token'] == $_SESSION['token']){
+  $age_token = $_SESSION['token_time'] - time();
+  if($age_token <= 60){ //limit the validity of token. counts in min? we give 30mins? 60*30=1800
+
+    //so if you don't have a session id, never login so, redirect to index.php
+    if(!isset($_SESSION['user_id'])){
+      header("Location: ../index.php");
+
+    }else{
+
+      //if the session has been set, the if else loop will check if user is an admin or not 
+      if($_SESSION['role'] == 'admin'){
+        //if admin, then redirect them to the admin page
+        header("Location: ../crudINDEX.php");
+      }else{
+        //if it is not an admin, it would be a normal user
+        //redirect to member page
+        header("Location: ../index.php");
+
+      
+    }
+
+  } 
+}
+}
+else{
+  //if the token is not set then means that the session token has expired.
+  header("Location: ../index.php?session=timeout");
+  
+}
+*/
+?>
 <!DOCTYPE html>
 <html>
 <title>HAVANA</title>
@@ -16,7 +52,7 @@
 
        <?php  
                 $query = "SELECT * FROM tbl_product ORDER BY id ASC";  
-                $result = mysqli_query($connect, $query);  
+                $result = mysqli_query($conn, $query);  
                 if(mysqli_num_rows($result) > 0)  
                 {  
                      while($row = mysqli_fetch_array($result))  
@@ -45,8 +81,8 @@
 
 
     <?php
-      if (isset($_SESSION['u_id'])) {
-        echo "You are logged in!";
+      if (isset($_SESSION['user_id'])) {
+        echo "$_SESSION['user_id']";
       }
     ?>
   </div>
